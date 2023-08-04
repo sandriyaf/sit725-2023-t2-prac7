21cff9f743d916cfb6a1ba6cde877c89d8351258
sandriyaf <sandriyafernandes10@gmail.com>
2023-08-04 10:20:00 +1000

    Retrieving values from DB
    -Creating mongodb database
    -using get & post functions to interact with server.

--- scripts.js
+++ scripts.js
@@ -1,46 +1,59 @@
-const cardList = [ {
-   title: "Kitten 2",
-   src: "images/kitten2.jpg",
-   link: "About Kitten 2",
-   desciption: "Demo desciption about kitten 2"
-},
-{
-   title: "Kitten 3",
-   src: "images/kitten3.jpg",
-   link: "About Kitten 3",
-   desciption: "Demo desciption about kitten 3"
-}
-];
 
 // function to add the cardList to html
 const addCards = (items) => {
 items.forEach(item => {
    let itemToAppend = '<div class="col s4 center-align">'+
-       '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.src+'">'+
+       '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
        '</div><div class="card-content">'+
        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
        '<div class="card-reveal">'+
-       '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-       '<p class="card-text">'+item.desciption+'</p>'+
+       '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+       '<p class="card-text">'+item.description+'</p>'+
        '</div></div></div>';
    $("#card-section").append(itemToAppend);
 });
 }
 
-const submitForm = () => {
+const formSubmitted = () => {
 let formData = {};
-formData.first_name = $('#first_name').val();
-formData.last_name = $('#last_name').val();
-formData.password = $('#password').val();
-formData.email = $('#email').val();
-console.log("Form Data Submitted: ", formData);
+formData.title = $('#title').val();
+formData.subTitle = $('#subTitle').val();
+formData.path = $('#path').val();
+formData.description = $('#description').val();
+console.log(formData);
+postCat(formData);
+}
+
+function postCat(cat){
+   $.ajax({
+      url:'/api/cat',
+      type:'POST',
+      data:cat,
+      success:(result)=>{
+         if(result.statusCode === 201){
+            alert('Cat post Successful');
+         }
+   
+      }
+   });
+}
+
+function getAllCats(){
+   $.get('/api/cats',(response)=>{
+      if(response.statusCode === 200){
+         addCards(response.data);
+
+      }
+
+
+   });
 }
 
 $(document).ready(function(){
 $('.materialboxed').materialbox();
-$('.modal').modal();
-addCards(cardList);
 $('#formSubmit').click(()=>{
-   submitForm();
-})
+   formSubmitted();
+});
+$('.modal').modal();
+getAllCats();
 });
